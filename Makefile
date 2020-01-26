1cda56db62efa3ea5311a55a1376f04d6f94ea87
Dendendelen <d.y.plotnikov@gmail.com>
2020-01-25 20:40:07 -0500

    Add files via upload

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-DEPS_CFLAGS?=$(shell env PKG_CONFIG_PATH=/usr/local/frc/lib/pkgconfig pkg-config                                                                                                              --cflags wpilibc)
-DEPS_LIBS?=$(shell env PKG_CONFIG_PATH=/usr/local/frc/lib/pkgconfig pkg-config -                                                                                                             -libs wpilibc)
+DEPS_CFLAGS?=$(shell env PKG_CONFIG_PATH=/usr/local/frc/lib/pkgconfig pkg-config --cflags wpilibc)
+DEPS_LIBS?=$(shell env PKG_CONFIG_PATH=/usr/local/frc/lib/pkgconfig pkg-config --libs wpilibc)
 EXE=multiCameraServerExample
 DESTDIR?=/home/pi/
 
@@ -8,15 +8,15 @@ DESTDIR?=/home/pi/
 build: ${EXE}
 
 install: build
-        cp ${EXE} runCamera ${DESTDIR}
+	cp ${EXE} runCamera ${DESTDIR}
 
 clean:
-        rm ${EXE} *.o
+	rm ${EXE} *.o
 
-OBJS=main.o
+OBJS=main.o ReflectiveTapeGripPipeline.o
 
 ${EXE}: ${OBJS}
-        ${CXX} -pthread -g -o $@ $^ ${DEPS_LIBS} -Wl,--unresolved-symbols=ignore                                                                                                             -in-shared-libs
+	${CXX} -pthread -g -o $@ $^ ${DEPS_LIBS} -Wl,--unresolved-symbols=ignore-in-shared-libs
 
 .cpp.o:
-        ${CXX} -pthread -g -Og -c -o $@ -std=c++17 ${CXXFLAGS} ${DEPS_CFLAGS} $<
+	${CXX} -pthread -g -Og -c -o $@ -std=c++17 ${CXXFLAGS} ${DEPS_CFLAGS} $<
---
+++ ReflectiveTapeGripPipeline.cpp
@@ -0,0 +1,217 @@
+#include "ReflectiveTapeGripPipeline.h"
+
+namespace grip {
+
+ReflectiveTapeGripPipeline::ReflectiveTapeGripPipeline() {
+}
+/**
+* Runs an iteration of the pipeline and updates outputs.
+*/
+void ReflectiveTapeGripPipeline::Process(cv::Mat& source0){
+	//Step RGB_Threshold0:
+	//input
+	cv::Mat rgbThresholdInput = source0;
+	double rgbThresholdRed[] = {0.0, 255.0};
+	double rgbThresholdGreen[] = {185.74640287769782, 255.0};
+	double rgbThresholdBlue[] = {222.43705035971223, 255.0};
+	rgbThreshold(rgbThresholdInput, rgbThresholdRed, rgbThresholdGreen, rgbThresholdBlue, this->rgbThresholdOutput);
+	//Step Blur0:
+	//input
+	cv::Mat blurInput = rgbThresholdOutput;
+	BlurType blurType = BlurType::GAUSSIAN;
+	double blurRadius = 0.0;  // default Double
+	blur(blurInput, blurType, blurRadius, this->blurOutput);
+	//Step CV_dilate0:
+	//input
+	cv::Mat cvDilateSrc = blurOutput;
+	cv::Mat cvDilateKernel;
+	cv::Point cvDilateAnchor(-1, -1);
+	double cvDilateIterations = 4.0;  // default Double
+    int cvDilateBordertype = cv::BORDER_REFLECT_101;
+	cv::Scalar cvDilateBordervalue(-1);
+	cvDilate(cvDilateSrc, cvDilateKernel, cvDilateAnchor, cvDilateIterations, cvDilateBordertype, cvDilateBordervalue, this->cvDilateOutput);
+	//Step CV_erode0:
+	//input
+	cv::Mat cvErodeSrc = cvDilateOutput;
+	cv::Mat cvErodeKernel;
+	cv::Point cvErodeAnchor(-1, -1);
+	double cvErodeIterations = 3.0;  // default Double
+    int cvErodeBordertype = cv::BORDER_REPLICATE;
+	cv::Scalar cvErodeBordervalue(-1);
+	cvErode(cvErodeSrc, cvErodeKernel, cvErodeAnchor, cvErodeIterations, cvErodeBordertype, cvErodeBordervalue, this->cvErodeOutput);
+	//Step Find_Lines0:
+	//input
+	cv::Mat findLinesInput = cvErodeOutput;
+	findLines(findLinesInput, this->findLinesOutput);
+	//Step Filter_Lines0:
+	//input
+	std::vector<Line> filterLinesLines = findLinesOutput;
+	double filterLinesMinLength = 10.0;  // default Double
+	double filterLinesAngle[] = {0.0, 360.0};
+	filterLines(filterLinesLines, filterLinesMinLength, filterLinesAngle, this->filterLinesOutput);
+}
+
+/**
+ * This method is a generated getter for the output of a RGB_Threshold.
+ * @return Mat output from RGB_Threshold.
+ */
+cv::Mat* ReflectiveTapeGripPipeline::GetRgbThresholdOutput(){
+	return &(this->rgbThresholdOutput);
+}
+/**
+ * This method is a generated getter for the output of a Blur.
+ * @return Mat output from Blur.
+ */
+cv::Mat* ReflectiveTapeGripPipeline::GetBlurOutput(){
+	return &(this->blurOutput);
+}
+/**
+ * This method is a generated getter for the output of a CV_dilate.
+ * @return Mat output from CV_dilate.
+ */
+cv::Mat* ReflectiveTapeGripPipeline::GetCvDilateOutput(){
+	return &(this->cvDilateOutput);
+}
+/**
+ * This method is a generated getter for the output of a CV_erode.
+ * @return Mat output from CV_erode.
+ */
+cv::Mat* ReflectiveTapeGripPipeline::GetCvErodeOutput(){
+	return &(this->cvErodeOutput);
+}
+/**
+ * This method is a generated getter for the output of a Find_Lines.
+ * @return LinesReport output from Find_Lines.
+ */
+std::vector<Line>* ReflectiveTapeGripPipeline::GetFindLinesOutput(){
+	return &(this->findLinesOutput);
+}
+/**
+ * This method is a generated getter for the output of a Filter_Lines.
+ * @return LinesReport output from Filter_Lines.
+ */
+std::vector<Line>* ReflectiveTapeGripPipeline::GetFilterLinesOutput(){
+	return &(this->filterLinesOutput);
+}
+	/**
+	 * Segment an image based on color ranges.
+	 *
+	 * @param input The image on which to perform the RGB threshold.
+	 * @param red The min and max red.
+	 * @param green The min and max green.
+	 * @param blue The min and max blue.
+	 * @param output The image in which to store the output.
+	 */
+	void ReflectiveTapeGripPipeline::rgbThreshold(cv::Mat &input, double red[], double green[], double blue[], cv::Mat &output) {
+		cv::cvtColor(input, output, cv::COLOR_BGR2RGB);
+		cv::inRange(output, cv::Scalar(red[0], green[0], blue[0]), cv::Scalar(red[1], green[1], blue[1]), output);
+	}
+
+	/**
+	 * Softens an image using one of several filters.
+	 *
+	 * @param input The image on which to perform the blur.
+	 * @param type The blurType to perform.
+	 * @param doubleRadius The radius for the blur.
+	 * @param output The image in which to store the output.
+	 */
+	void ReflectiveTapeGripPipeline::blur(cv::Mat &input, BlurType &type, double doubleRadius, cv::Mat &output) {
+		int radius = (int)(doubleRadius + 0.5);
+		int kernelSize;
+		switch(type) {
+			case BOX:
+				kernelSize = 2 * radius + 1;
+				cv::blur(input,output,cv::Size(kernelSize, kernelSize));
+				break;
+			case GAUSSIAN:
+				kernelSize = 6 * radius + 1;
+				cv::GaussianBlur(input, output, cv::Size(kernelSize, kernelSize), radius);
+				break;
+			case MEDIAN:
+				kernelSize = 2 * radius + 1;
+				cv::medianBlur(input, output, kernelSize);
+				break;
+			case BILATERAL:
+				cv::bilateralFilter(input, output, -1, radius, radius);
+				break;
+        }
+	}
+	/**
+	 * Expands area of higher value in an image.
+	 * @param src the Image to dilate.
+	 * @param kernel the kernel for dilation.
+	 * @param anchor the center of the kernel.
+	 * @param iterations the number of times to perform the dilation.
+	 * @param borderType pixel extrapolation method.
+	 * @param borderValue value to be used for a constant border.
+	 * @param dst Output Image.
+	 */
+	void ReflectiveTapeGripPipeline::cvDilate(cv::Mat &src, cv::Mat &kernel, cv::Point &anchor, double iterations, int borderType, cv::Scalar &borderValue, cv::Mat &dst) {
+		cv::dilate(src, dst, kernel, anchor, (int)iterations, borderType, borderValue);
+	}
+
+	/**
+	 * Expands area of lower value in an image.
+	 * @param src the Image to erode.
+	 * @param kernel the kernel for erosion.
+	 * @param anchor the center of the kernel.
+	 * @param iterations the number of times to perform the erosion.
+	 * @param borderType pixel extrapolation method.
+	 * @param borderValue value to be used for a constant border.
+	 * @param dst Output Image.
+	 */
+	void ReflectiveTapeGripPipeline::cvErode(cv::Mat &src, cv::Mat &kernel, cv::Point &anchor, double iterations, int borderType, cv::Scalar &borderValue, cv::Mat &dst) {
+		cv::erode(src, dst, kernel, anchor, (int)iterations, borderType, borderValue);
+	}
+
+	/**
+	 * Finds all line segments in an image.
+	 *
+	 * @param input The image on which to perform the find lines.
+	 * @param lineList The output where the lines are stored.
+	 */
+	void ReflectiveTapeGripPipeline::findLines(cv::Mat &input, std::vector<Line> &lineList) {
+		cv::Ptr<cv::LineSegmentDetector> lsd = cv::createLineSegmentDetector(1);
+		std::vector<cv::Vec4i> lines;
+		lineList.clear();
+		if (input.channels() == 1) {
+			lsd->detect(input, lines);
+		} else {
+			// The line detector works on a single channel.
+			cv::Mat tmp;
+			cv::cvtColor(input, tmp, cv::COLOR_BGR2GRAY);
+			lsd->detect(tmp, lines);
+		}
+		// Store the lines in the LinesReport object
+		if (!lines.empty()) {
+			for (int i = 0; i < lines.size(); i++) {
+				cv::Vec4i line = lines[i];
+				lineList.push_back(Line(line[0], line[1], line[2], line[3]));
+			}
+		}
+	}
+
+	/**
+	 * Filters out lines that do not meet certain criteria.
+	 *
+	 * @param inputs The lines that will be filtered.
+	 * @param minLength The minimum length of a line to be kept.
+	 * @param angle The minimum and maximum angle of a line to be kept.
+	 * @param outputs The output lines after the filter.
+	 */
+	void ReflectiveTapeGripPipeline::filterLines(std::vector<Line> &inputs, double minLength, double angle[], std::vector<Line> &outputs) {
+	outputs.clear();
+	for (Line line: inputs) {
+		if (line.length()>abs(minLength)) {
+			if ((line.angle() >= angle[0] && line.angle() <= angle[1]) ||
+					(line.angle() + 180.0 >= angle[0] && line.angle() + 180.0 <=angle[1])) {
+				outputs.push_back(line);
+			}
+		}
+	}
+	}
+
+
+
+} // end grip namespace
+
